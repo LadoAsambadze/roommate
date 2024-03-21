8562547d925ee4bf960941455ce52fd1978eeef6
Lado Asambadze <lado.asambadze1@gmail.com>
2024-03-21 16:36:00 +0400

    install prettier and prettier tailwind plugin for styles

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,15 +1,11 @@
-import i18nConfig from "@/i18nConfig";
 import "./globals.css";
-import type { Metadata } from "next";
-import { Inter, Noto_Serif_Georgian, Roboto } from "next/font/google";
+// import type { Metadata } from "next";
+import { Noto_Serif_Georgian } from "next/font/google";
 import { ReactNode } from "react";
-import { Sansita } from "next/font/google";
-
 import { dir } from "i18next";
 import TranslationsProvider from "@/components/translation/TranslationsProvider";
 import initTranslations from "../i18n";
 import localfont from "next/font/local";
-import { Fira_Sans } from "next/font/google";
 import { Italianno } from "next/font/google";
 
 const italianno = Italianno({
--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,21 +1,21 @@
-import Link from "next/link";
-import initTranslations from "../i18n";
-import ExampleClientComponent from "@/components/ExaplmeClientComponent";
-import { BankIcon } from "@/components/svg";
+import Link from 'next/link'
+import initTranslations from '../i18n'
+import ExampleClientComponent from '@/components/ExaplmeClientComponent'
+import { BankIcon } from '@/components/svg'
 
-const i18nNamespaces = ["home"];
+const i18nNamespaces = ['home']
 
 async function Home({ params: { locale } }: { params: { locale: string } }) {
-  const { t } = await initTranslations(locale, i18nNamespaces);
+    const { t } = await initTranslations(locale, i18nNamespaces)
 
-  return (
-    <main>
-      <h1 className="">{t("hello")}</h1>
-      <Link href="/about">{t("hello")}</Link>
-      <ExampleClientComponent />
-      <BankIcon className="" />
-    </main>
-  );
+    return (
+        <main>
+            <h1 className="w-full text-xl hover:text-white">{t('hello')}</h1>
+            <Link href="/about">{t('hello')}</Link>
+            <ExampleClientComponent />
+            <BankIcon className="" />
+        </main>
+    )
 }
 
-export default Home;
+export default Home
