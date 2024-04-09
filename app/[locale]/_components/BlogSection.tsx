98c52c35609b36a80486d97d6af076da4b0aca8b
Lado Asambadze <lado.asambadze1@gmail.com>
2024-04-09 12:07:27 +0400

    add blog section to home page

--- app/[locale]/_components/BlogSection.tsx
+++ app/[locale]/_components/BlogSection.tsx
@@ -14,6 +14,7 @@ import { useTranslation } from 'react-i18next'
 import BlogFirst from '../../../public/images/BlogFirst.png'
 import BlogSecond from '../../../public/images/BlogSecond.png'
 import BlogThirth from '../../../public/images/BlogThirth.png'
+import { useMediaQuery } from 'react-responsive'
 
 export default function BlogSection() {
     const { t } = useTranslation()
@@ -24,8 +25,8 @@ export default function BlogSection() {
             image: BlogFirst,
             buttonText: t('findMore'),
             link: 'https://www.roommate.blog/',
-            width: 157,
-            height: 144,
+            width: 110,
+            height: 200,
         },
         {
             header: t('blogHeader2'),
@@ -42,16 +43,14 @@ export default function BlogSection() {
             image: BlogThirth,
             buttonText: t('findMore'),
             link: 'https://www.roommate.blog/',
-            width: 157,
+            width: 200,
             height: 144,
         },
     ]
-    // const media = useMediaQuery({
-    //     query: '(min-width: 768px) and (max-width: 1280px)',
-    // })
-    // const dragMedia = useMediaQuery({
-    //     query: '(min-width: 0px) and (max-width: 1280px)',
-    // })
+
+    const dragMedia = useMediaQuery({
+        query: '(min-width: 0px) and (max-width: 1280px)',
+    })
     return (
         <section className="my-12 flex h-full w-full flex-col items-start px-6 sm:px-16 md:px-20 lg:my-24 xl:px-24">
             <h1 className="text-2xl text-[#484848]">{t('blogMainHead')}</h1>
@@ -59,17 +58,17 @@ export default function BlogSection() {
             <Carousel
                 opts={{
                     align: 'start',
-                    watchDrag: true,
+                    watchDrag: dragMedia,
                 }}
                 className="mt-6 w-full p-0  "
             >
-                <CarouselContent className="  pr-10  lg:pr-16 xl:pr-0">
+                <CarouselContent className="pr-6  lg:pr-16 xl:pr-0">
                     {data.map((item, index) => (
                         <CarouselItem key={index} className="w-full md:basis-1/2 xl:basis-1/3">
-                            <div className="relative flex  h-full w-full flex-row justify-between overflow-hidden rounded-xl bg-[#c0dbfc]   pb-10 pl-6 pr-20 pt-4 lg:pb-10 lg:pl-8 lg:pr-[140px] lg:pt-8 ">
-                                <div className='flex flex-col'>
+                            <div className="relative flex  h-full w-full flex-row  overflow-hidden rounded-xl bg-[#c0dbfc]   pb-10 pl-5 pr-[70px] pt-4 sm:pr-20 lg:pb-10 lg:pl-8 lg:pr-[140px] lg:pt-8 ">
+                                <div className="flex flex-col">
                                     <span className="text-base font-semibold">{item.header}</span>
-                                    <div className="mt-4 flex  h-full flex-col justify-between">
+                                    <div className="mt-2 flex h-full  flex-col justify-end md:mt-2 md:justify-end">
                                         <span className="z-50 text-xs ">{item.text}</span>
                                         <Link href={item.link}>
                                             <Button className="bottom-0 z-50 mt-4 w-32 text-xs">
@@ -79,9 +78,9 @@ export default function BlogSection() {
                                     </div>
                                 </div>
                                 <Image
-                                    className=" bottom-0 right-0"
-                                    width={157}
-                                    height={50}
+                                    className="absolute -right-4 bottom-0 md:right-0"
+                                    width={item.width}
+                                    height={item.height}
                                     src={item.image}
                                     alt="123"
                                 />
@@ -89,10 +88,10 @@ export default function BlogSection() {
                         </CarouselItem>
                     ))}
                 </CarouselContent>
-                <div className="hidden xl:block">
+                <div className="hidden md:block xl:hidden">
                     <CarouselPrevious />
                 </div>
-                <div className="hidden xl:block">
+                <div className="hidden md:block xl:hidden">
                     <CarouselNext />
                 </div>
             </Carousel>
