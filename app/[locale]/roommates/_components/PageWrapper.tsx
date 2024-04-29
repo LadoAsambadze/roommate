5f56147f39c25f07fbc042222e5c83912b74c767
Lado Asambadze <lado.asambadze1@gmail.com>
2024-04-29 13:41:48 +0400

    update styles on roomates page

--- app/[locale]/roommates/_components/PageWrapper.tsx
+++ app/[locale]/roommates/_components/PageWrapper.tsx
@@ -3,25 +3,29 @@
 import React, { useState } from 'react'
 import Filter from './filter/Filter'
 import UserCard from './userCard/UserCard'
-import { Button } from '@/components/ui/button'
 import MobileFilter from './filter/MobileFilter'
+import { FilterIcon } from '@/components/svgs'
 
 export default function PageWrapper() {
     const [isOpen, setIsOpen] = useState(false)
     return (
         <>
-            <main className="flex min-h-screen w-full flex-col  gap-4  md:flex-row md:gap-6 md:px-24 md:py-10">
-                <div className="h-auto w-full px-6 pt-4 sm:px-32 md:hidden">
-                    <Button onClick={() => setIsOpen(!isOpen)} className="  flex md:hidden">
-                        Filter
-                    </Button>
+            <main className="flex min-h-screen w-full flex-col  gap-4  xl:flex-row md:gap-6 md:px-24 md:py-10">
+                <div className="h-auto w-full px-6 pt-4 sm:pl-32  md:pl-24 lg:pl-0 xl:hidden">
+                    <button
+                        onClick={() => setIsOpen(!isOpen)}
+                        className="flex  flex-row items-center  rounded-lg border border-[#838CAC] bg-[#F2F5FF] px-4 py-2 "
+                    >
+                        <FilterIcon className="h-6 w-6" />
+                        <span className="ml-2 text-sm text-[#838CAC]">Filter</span>
+                    </button>
                 </div>
-                <div className="hidden h-full w-1/4 md:block">
+                <div className="hidden h-full w-1/4 xl:block">
                     <Filter isOpen={isOpen} setIsOpen={setIsOpen} />
                 </div>
                 <MobileFilter isOpen={isOpen} setIsOpen={setIsOpen} />
 
-                <div className="hidden h-screen w-[1px] bg-[#E3E3E3] md:block"></div>
+                <div className="hidden h-screen w-[1px] bg-[#E3E3E3] xl:block"></div>
                 <UserCard />
             </main>
         </>
--- app/[locale]/roommates/_components/userCard/UserCard.tsx
+++ app/[locale]/roommates/_components/userCard/UserCard.tsx
@@ -1,20 +1,19 @@
 import Image from 'next/image'
 import Temporary from '../../../../../public/images/Temporary.jpg'
-import { Button } from '@/components/ui/button'
 import Avatar from '../../../../../public/images/MaleAvatar.jpg'
-import { Heart, Location } from '@/components/svgs'
+import { Heart, Location, Sms } from '@/components/svgs'
 
 export default function UserCard() {
     return (
         <>
-            <section className="flex min-h-screen w-full flex-col gap-6 px-6 sm:px-32 md:w-auto md:px-0 ">
-                <div className="flex h-auto  w-full flex-col gap-6  overflow-hidden rounded-lg bg-[#FFFFFF]  shadow-md  md:h-[232px] md:w-full md:flex-row md:p-4 ">
+            <section className="flex min-h-screen w-full flex-col gap-6 px-6 sm:px-32 md:w-auto md:px-24  lg:px-0">
+            <div className="flex h-auto  w-full flex-col gap-6  overflow-hidden rounded-lg bg-[#FFFFFF]  shadow-md  lg:h-[232px] lg:w-full lg:flex-row lg:p-4 ">
                     <Image
                         src={Avatar}
-                        className="h-[200px] w-full rounded-lg  object-cover md:h-full md:w-[332px] "
+                        className="h-[200px] w-full rounded-lg  object-cover lg:h-full  lg:w-[332px]"
                         alt="test"
                     />
-                    <div className="flex h-full w-full flex-col gap-4 pb-3 pt-4 md:p-0">
+                    <div className="flex h-full w-full flex-col gap-4 p-4 pb-3 pt-4 lg:p-0">
                         <div className="flex w-full flex-row  items-start justify-between">
                             <div className="flex flex-col gap-1 md:flex-row">
                                 <span className="text-base font-semibold md:text-sm">
@@ -22,7 +21,10 @@ export default function UserCard() {
                                 </span>
                                 <span className="text-sm text-[#838CAC]">16.04.2022</span>
                             </div>
-                            <Button>Message</Button>
+                            <button className="flex flex-row items-center rounded-md bg-[#0A7CFF] px-3 py-2">
+                                <Sms className="h-4 w-4" />
+                                <span className="ml-2 text-sm text-white">message</span>
+                            </button>
                         </div>
                         <div className="hidden h-[1px] w-full bg-[#E3E3E3] md:block"></div>
                         <span className="text-sm">
@@ -53,13 +55,14 @@ export default function UserCard() {
                     </div>
                 </div>
 
-                <div className="flex h-auto  w-full flex-col gap-6  overflow-hidden rounded-lg bg-[#FFFFFF]  shadow-md  md:h-[232px] md:w-full md:flex-row md:p-4 ">
+
+                <div className="flex h-auto  w-full flex-col gap-6  overflow-hidden rounded-lg bg-[#FFFFFF]  shadow-md  lg:h-[232px] lg:w-full lg:flex-row lg:p-4 ">
                     <Image
                         src={Temporary}
-                        className="h-[200px] w-full rounded-lg  object-cover md:h-full md:w-[332px] "
+                        className="h-[200px] w-full rounded-lg  object-cover lg:h-full  lg:w-[332px]"
                         alt="test"
                     />
-                    <div className="flex h-full w-full flex-col gap-4 pb-3 pt-4 md:p-0">
+                    <div className="flex h-full w-full flex-col gap-4 p-4 pb-3 pt-4 lg:p-0">
                         <div className="flex w-full flex-row  items-start justify-between">
                             <div className="flex flex-col gap-1 md:flex-row">
                                 <span className="text-base font-semibold md:text-sm">
@@ -67,7 +70,10 @@ export default function UserCard() {
                                 </span>
                                 <span className="text-sm text-[#838CAC]">16.04.2022</span>
                             </div>
-                            <Button>Message</Button>
+                            <button className="flex flex-row items-center rounded-md bg-[#0A7CFF] px-3 py-2">
+                                <Sms className="h-4 w-4" />
+                                <span className="ml-2 text-sm text-white">message</span>
+                            </button>
                         </div>
                         <div className="hidden h-[1px] w-full bg-[#E3E3E3] md:block"></div>
                         <span className="text-sm">
@@ -97,6 +103,7 @@ export default function UserCard() {
                         </div>
                     </div>
                 </div>
+
             </section>
         </>
     )
