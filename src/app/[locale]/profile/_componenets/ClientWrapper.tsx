0dd4375d6d14ceb808c56998c8ec26f6e05efead
Lado Asambadze <lado.asambadze1@gmail.com>
2024-09-21 22:08:22 +0400

    update profile comp

--- src/app/[locale]/profile/_componenets/ClientWrapper.tsx
+++ src/app/[locale]/profile/_componenets/ClientWrapper.tsx
@@ -16,7 +16,7 @@ function ClientWrapper() {
 
     return (
         <main className="mt-1 flex min-h-screen w-full flex-col  items-center overflow-scroll bg-[#F5F5F5] px-6 pb-8 pt-6 md:flex-row md:items-start md:gap-6 md:px-24">
-            {verification === 'true' ? <Verification /> : <ProfileNavBar />}
+            {verification === 'true' && isMobile ? <Verification /> : <ProfileNavBar />}
         </main>
     )
 }
